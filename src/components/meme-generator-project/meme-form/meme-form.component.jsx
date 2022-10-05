1e4758766ca5ad16a516f033942fb5162942cad6
Ali Kri <alinanosovets6849@gmail.com>
2022-10-05 12:37:14 +0200

    finished meme generator

--- src/components/meme-generator-project/meme-form/meme-form.component.jsx
+++ src/components/meme-generator-project/meme-form/meme-form.component.jsx
@@ -1,6 +1,6 @@
 import "./meme-form.styles.scss";
 import memeData from "../../../memesData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const MemeForm = () => {
@@ -12,11 +12,27 @@ const MemeForm = () => {
         randomImage: "" 
 		}
 	)
-	const [memeImages, setMemeImages] = useState(memeData);
+	const [memeImages, setMemeImages] = useState([]);
+
+	useEffect(() => {
+		getData();
+	})
+
+	async function getData() {
+		const response = await fetch("https://api.imgflip.com/get_memes");
+		const data = await response.json();
+		setMemeImages(data.data.memes);
+	}
+	const handleChange = (event) => {
+		const {name, value } = event.target
+		setMeme(prevValue => ({
+			...prevValue,
+			[name]: value
+		}))
+	}
 	
 	const randomUrl = () => {
-		const memeArray = memeImages.data.memes; 
-		const memeImage = memeArray[Math.floor(Math.random() * memeArray.length)].url;
+		const memeImage = memeImages[Math.floor(Math.random() * memeImages.length)].url;
 		setMeme((prevValue) => (
 			{
 				...prevValue,
@@ -30,12 +46,29 @@ const MemeForm = () => {
 	return (
 		<main>
 			<div className="meme-form">
-				<input type="text" className="meme-form-input" placeholder="Top text"/>
-				<input type="text" className="meme-form-input"placeholder="Bottom text"/>
+				<input
+					type="text"
+					className="meme-form-input"
+					placeholder="Top text"
+					name="topText"
+					value={meme.topText}
+					onChange={handleChange}
+				/>
+				<input
+					type="text"
+					className="meme-form-input"
+					placeholder="Bottom text"
+					name="bottomText"
+					value={meme.bottomText}
+					onChange={handleChange}
+				/>
 				<button className="meme-form-btn" onClick={randomUrl}>Get a new meme image 🖼</button>
 			</div>
 			<div className="meme-image-wrapper">
+				<h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
 				<img src={`${meme.randomImage}`} alt="" />
+				
 			</div>
 		</main>
 	)
